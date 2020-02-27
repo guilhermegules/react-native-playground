5d95cab464ae06a3803a4eed73c22971e38f9614
Guilherme Gules Moreira <guilherme.moreira@ilegra.com>
2020-02-27 19:23:20 -0300

    first commit

---
+++ TodoApp/components/AddTodo.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import {StyleSheet, Text, TextInput, Button, View} from 'react-native';
+
+const AddTodo = ({submitHandler}) => {
+  const [text, setText] = useState('');
+
+  const changeHandler = value => {
+    setText(value);
+  };
+
+  return (
+    <View>
+      <TextInput
+        style={styles.input}
+        placeholder="new todo..."
+        onChangeText={changeHandler}
+      />
+      <Button
+        onPress={() => submitHandler(text)}
+        title="Add todo"
+        color="coral"
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    marginBottom: 10,
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+  },
+});
+
+export default AddTodo;
